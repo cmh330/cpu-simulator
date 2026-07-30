4beceac740f20ba6fef8edeb17d0112adfcc64c8
cmh330 <1487735597@qq.com>
2026-07-30 14:55:28 +0800

    update alu: add branch

--- include/alu.h
+++ include/alu.h
@@ -2,7 +2,8 @@
 #include <cstdint>
 
 enum class AluOp{
-    ADD, SUB, AND, OR, XOR, SLL, SRL, SRA, SLT, SLTU
+    ADD, SUB, AND, OR, XOR, SLL, SRL, SRA, SLT, SLTU,
+    BEQ, BGE, BGEU, BLT, BLTU, BNE
 };
 
 inline int32_t compute(AluOp op, int32_t l, int32_t r) {
@@ -17,6 +18,14 @@ inline int32_t compute(AluOp op, int32_t l, int32_t r) {
         case AluOp::SRA: return l >> (r & 0x1F);
         case AluOp::SLT: return (l < r) ? 1 : 0;
         case AluOp::SLTU: return (static_cast<uint32_t>(l) < static_cast<uint32_t>(r)) ? 1 : 0;
+
+        // branch: 1跳转，0不跳转
+        case AluOp::BEQ: return (l == r) ? 1 : 0;
+        case AluOp::BGE: return (l >= r) ? 1 : 0;
+        case AluOp::BGEU: return (static_cast<uint32_t>(l) >= static_cast<uint32_t>(r)) ? 1 : 0;
+        case AluOp::BLT: return (l < r) ? 1 : 0;
+        case AluOp::BLTU: return (static_cast<uint32_t>(l) < static_cast<uint32_t>(r)) ? 1 : 0;
+        case AluOp::BNE: return (l != r) ? 1 : 0;
     }
     return 0;
 }
