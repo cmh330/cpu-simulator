fe6f041620a5d91fadff500498d6d4f13d254c71
cmh330 <1487735597@qq.com>
2026-07-31 10:32:15 +0800

    update register status: add step()

--- include/registerStatus.h
+++ include/registerStatus.h
@@ -3,13 +3,17 @@
 
 const int NO_PRODUCER = -1; // 没有指令在写这个寄存器
 const int NUM_REGISTERS = 32;
+const int NO_TAG = -1;
 
 class RegisterStatus{
 private:
-    int producer_tag[NUM_REGISTERS]; // tag是ROB队列中的下标
+    int producer_new[NUM_REGISTERS]; // tag是ROB队列中的下标
+    int producer_old[NUM_REGISTERS];
 public:
     RegisterStatus();
     int get_producer(int r) const;
-    void set_producer(int r, int tag);
-    void clear_if_match(int r, int tag);
+    // void set_producer(int r, int tag);
+    // void clear_if_match(int r, int tag);
+    void step(int broadcast_tag, int issue_reg, int issue_tag); // issue_reg/issue_tag: 发布的新指令写哪个寄存器、标签是什么
+    void sync();
 };
--- src/registerStatus.cpp
+++ src/registerStatus.cpp
@@ -2,14 +2,16 @@
 
 RegisterStatus::RegisterStatus() {
     for (int i = 0; i < NUM_REGISTERS; ++i) {
-        producer_tag[i] = NO_PRODUCER;
+        producer_new[i] = NO_PRODUCER;
+        producer_old[i] = NO_PRODUCER;
     }
 }
 
 int RegisterStatus::get_producer(int r) const {
-    return producer_tag[r];
+    return producer_old[r];
 }
 
+/*
 void RegisterStatus::set_producer(int r, int tag) {
     if (r == 0) return;
     producer_tag[r] = tag;
@@ -18,5 +20,30 @@ void RegisterStatus::set_producer(int r, int tag) {
 void RegisterStatus::clear_if_match(int r, int tag) {
     if (producer_tag[r] == tag) {
         producer_tag[r] = NO_PRODUCER;
+    }
+}
+*/
+
+void RegisterStatus::sync() {
+    for (int i = 0; i < NUM_REGISTERS; ++i) {
+        producer_old[i] = producer_new[i];
+    }
+}
+
+void RegisterStatus::step(int broadcast_tag, int issue_reg, int issue_tag) {
+    for (int i = 0; i <  NUM_REGISTERS; ++i) {
+        producer_new[i] = producer_old[i];
+    }
+
+    if (broadcast_tag != NO_TAG) {
+        for (int i = 0; i < NUM_REGISTERS; ++i) {
+            if (producer_old[i] == broadcast_tag) {
+                producer_new[i] = NO_TAG;
+            }
+        }
+    }
+
+    if (issue_tag != NO_TAG && issue_reg != 0) {
+        producer_new[issue_reg] = issue_tag;
     }
 }
