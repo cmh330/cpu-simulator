56773f91ee51e943bf0fce14b25f9797ab4d8005
cmh330 <1487735597@qq.com>
2026-08-02 18:18:49 +0800

    update register status

--- include/registerStatus.h
+++ include/registerStatus.h
@@ -1,19 +1,21 @@
 #pragma once
 #include <cstdint>
+#include "ROB.h"
 
 const int NO_PRODUCER = -1; // 没有指令在写这个寄存器
 const int NUM_REGISTERS = 32;
-const int NO_TAG = -1;
+// const int NO_TAG = -1;
 
 class RegisterStatus{
 private:
     int producer_new[NUM_REGISTERS]; // tag是ROB队列中的下标
     int producer_old[NUM_REGISTERS];
+    static int age_distance(int tag, int rob_head);
 public:
     RegisterStatus();
     int get_producer(int r) const;
     // void set_producer(int r, int tag);
     // void clear_if_match(int r, int tag);
-    void step(int broadcast_tag, int issue_reg, int issue_tag); // issue_reg/issue_tag: 发布的新指令写哪个寄存器、标签是什么
+    void step(int broadcast_tag, int issue_reg, int issue_tag, int flush_tag, int rob_head); // issue_reg/issue_tag: 发布的新指令写哪个寄存器、标签是什么
     void sync();
 };
--- src/registerStatus.cpp
+++ src/registerStatus.cpp
@@ -24,13 +24,17 @@ void RegisterStatus::clear_if_match(int r, int tag) {
 }
 */
 
+int RegisterStatus::age_distance(int tag, int rob_head) {
+    return (tag - rob_head + ROB_SIZE) % ROB_SIZE;
+}
+
 void RegisterStatus::sync() {
     for (int i = 0; i < NUM_REGISTERS; ++i) {
         producer_old[i] = producer_new[i];
     }
 }
 
-void RegisterStatus::step(int broadcast_tag, int issue_reg, int issue_tag) {
+void RegisterStatus::step(int broadcast_tag, int issue_reg, int issue_tag, int flush_tag, int rob_head) {
     for (int i = 0; i <  NUM_REGISTERS; ++i) {
         producer_new[i] = producer_old[i];
     }
@@ -45,5 +49,16 @@ void RegisterStatus::step(int broadcast_tag, int issue_reg, int issue_tag) {
 
     if (issue_tag != NO_TAG && issue_reg != 0) {
         producer_new[issue_reg] = issue_tag;
+    }
+
+    // flush
+    if (flush_tag != NO_TAG) {
+        int flush_dist = age_distance(flush_tag, rob_head);
+        for (int i = 0; i < NUM_REGISTERS; ++i) {
+            if (producer_old[i] == NO_PRODUCER) continue;
+            if (age_distance(producer_old[i], rob_head) > flush_dist) {
+                producer_new[i] = NO_PRODUCER;
+            }
+        }
     }
 }
